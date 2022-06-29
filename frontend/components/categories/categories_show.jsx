3f7cce1bc07d9d549f3b9c3928c19d6301f3631c
apodavonov3616 <apodavonov3616@DESKTOP-DU4IAMR.localdomain>
2022-06-29 14:16:35 -0400

    added functioning buttons

---
+++ frontend/components/categories/categories_show.jsx
@@ -0,0 +1,32 @@
+import React, { useEffect } from 'react';
+import MedicationIndexItem from '../medication/medication_index_item';
+
+const CategoryShow = (props) => {
+
+    useEffect(() => {
+        fetchMedications();
+    }, []);
+
+    const { medications, fetchMedications } = props
+
+    const filterFunction = () => {
+        return props.medications.filter((medication) => {
+            return (medication.category_name.toLowerCase().includes(props.category))
+        })
+    };
+
+    return (
+        <div className="category-result-container">
+            <ul className="category-result">
+                {
+                    filterFunction().map(medication => <MedicationIndexItem
+                        medication={medication}
+                        key={medication.id}
+                    />)
+                }
+            </ul>
+        </div>
+    )
+}
+
+export default CategoryShow;
